b66b67486a95427f103aaabb35775e8cdfd25d49
Kotta64 <k21073kk@aitech.ac.jp>
2022-12-15 13:52:00 +0900

    画像処理：テンプレートの追加

---
+++ watch.py
@@ -0,0 +1,14 @@
+import cv2
+
+class Image_Processing():
+    def __init__(self, image):
+        self.image = image
+    def img2gray(self):
+        #画像のグレイスケール化
+        pass
+    def face_detection(self):
+        #顔検出
+        pass
+    def contour_detection(self):
+        #輪郭抽出
+        pass
